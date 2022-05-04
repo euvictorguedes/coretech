90932551c2b2647fbcc3d89da30db5160d380149
Victor Guedes <victorfelipe.guedes@hotmail.com>
2022-05-03 23:34:34 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>coretech</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+    </>
+  )
+}
+
+export default Home
